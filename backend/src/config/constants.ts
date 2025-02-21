498c04f639fa14f6e57ba57e40d5ddc59d492cfe
gaspermerela <gmerela0@gmail.com>
2025-02-21 14:09:20 +0100

    feat(backend): Estimate blob aggregator execution transaction fee
    - Added logic for estimating the blob aggregator execution fee based on median gas price and `BLOB_AGG_TX_GAS_USED_ESTIMATE`.
    - Improved docs

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,2 +1,14 @@
 export const GAS_PER_BLOB = BigInt(32 * 4096);
-export const BLOB_AGG_TX_GAS_USED_ESTIMATE = 500000n
+
+/**
+ * Estimated execution gas usage for a blob aggregation transaction.
+ *
+ * Since the blob aggregation contract is not yet implemented,
+ * we use a conservative high estimate based on the average execution gas usage of other blob submitters.
+ * This ensures sufficient gas allocation to prevent underestimation.
+ *
+ * This estimate is derived from Dune queries:
+ *  - [Average Execution Gas Usage by Blob Submitters](https://dune.com/queries/4711922/7831269)
+ *  - [Execution Gas Usage Distribution for Blob Submitters](https://dune.com/queries/4712158/7831324)
+ */
+export const BLOB_AGG_TX_GAS_USED_ESTIMATE = 500000n;
--- backend/src/core/BlobDataService.ts
+++ backend/src/core/BlobDataService.ts
@@ -3,9 +3,9 @@ import {Block, TransactionEIP4844} from "viem";
 import {BlobInfo, BlockWithBlobs} from "./types";
 import {failure, Result, success} from "./result";
 import {beaconClient} from "../api/axios";
-import {isTransactionArray, timestampToSlotNumber} from "./utils";
+import {getMedianFee, isTransactionArray, timestampToSlotNumber} from "./utils";
 import {provider} from "../config/viem";
-import {GAS_PER_BLOB} from "../config/constants";
+import {BLOB_AGG_TX_GAS_USED_ESTIMATE, GAS_PER_BLOB} from "../config/constants";
 import {instanceToPlain, plainToInstance} from "class-transformer";
 import {BeaconBlobSidecarResponse} from "../api/models";
 
@@ -13,16 +13,14 @@ const BLOB_SIDECAR_RETRY_INTERVAL_MS = 1000;
 const BLOB_SIDECAR_MAX_ATTEMPTS = 5;
 
 export class BlobDataService {
-    private readonly historyFile: string;
-    private readonly historyRetentionSec: number;
-    private readonly namedAddresses: Map<string, string>;
-    private latestBlobFee: bigint = BigInt(0);
+    // Store the latest average blob fee to estimate the next blob fee even when the current block does not have any blob transactions
+    private latestAvgBlobFee: bigint = -1n;
     private senderReceiverToId: Map<string, bigint> = new Map();
 
     constructor(
-        historyFile: string,
-        historyRetentionSec: number,
-        namedAddresses: Map<string, string>
+        private readonly historyFile: string,
+        private readonly historyRetentionSec: number,
+        private readonly namedAddresses: Map<string, string>
     ) {
         this.historyFile = historyFile;
         this.historyRetentionSec = historyRetentionSec;
@@ -31,20 +29,21 @@ export class BlobDataService {
 
     /**
      * Processes a block with EIP-4844 blob transactions:
-     *  - Fetches blob sidecars and for each calculates:
-     *      - blob and execution fee per blob
-     *      - actual blob size
-     *      - versioned hash
-     * - Estimates next blob fee (excluding execution fee). Currently, it is the latest blob fee.
+     *  - For each blob sidecar calculates:
+     *      - Blob and execution fee per blob
+     *      - Actual blob size
+     *      - Versioned hash
+     * - Estimates next blob fee (excluding execution fee).
+     *      - Currently, this is based on the average blob fee of the current block. Consider calculating this using `baseFeePerBlobGas` and `blobGasUsedRatio` from `eth_feeHistory`.
      * - Estimates aggregator transaction execution fee (excluding blob fees).
-     *      This fee is estimated based on the median gas fee and BLOB_AGG_TX_GAS_USED_ESTIMATE.
-     *      If fetching median fee fails, this value is zero.
+     *      - This is based on the median gas fee and a constant `BLOB_AGG_TX_GAS_USED_ESTIMATE`.
      * - Saves the block with blob data to the history file.
      *
      * @param block - The block to process.
-     * @returns A `Result` with the processed `BlockWithBlobs` or an error.
+     * @returns {@link BlockWithBlobs} {@link Result} or an error.
      */
     public async processBlock(block: Block): Promise<Result<BlockWithBlobs, Error>> {
+        console.log(`Processing block ${block.number}`);
         if (block.number == null) {
             return failure(new Error('Block number is null'));
         }
@@ -61,44 +60,52 @@ export class BlobDataService {
         const blobSidecarsResponse = await this.getBlobSidecarsWithRetries(slotNumber);
         if (blobSidecarsResponse.isFailure()) {
             // If we failed to fetch blob sidecars after the maximum number of attempts, return an error
-            return blobSidecarsResponse
+            return blobSidecarsResponse;
         }
-        const blobSidecars = blobSidecarsResponse.unwrap()
+        const blobSidecars = blobSidecarsResponse.unwrap();
 
         // For each blob transaction, find the corresponding blob sidecars and initialize BlobInfo[]
         const blobInfos: BlobInfo[] = [];
+        const blobFees: bigint[] = []; // Used to calculate average blob fee of the current block, to estimate next blob fee
+
         for (const blobTx of blobTxs) {
             if (blobTx.blobVersionedHashes.length == 0) {
+                // Should not happen
                 return failure(new Error(`Blob transaction ${blobTx.hash} does not have any blob versioned hashes`));
             }
 
             const txBlobs = blobSidecars.data.filter(sidecar => blobTx.blobVersionedHashes.includes(sidecar.getBlobVersionedHash()));
             if (txBlobs.length != blobTx.blobVersionedHashes.length) {
+                // Should not happen
                 return failure(new Error(`Found ${txBlobs.length} blob sidecars for transaction ${blobTx.hash}, but expected ${blobTx.blobVersionedHashes.length} blobs`));
             }
 
             const receipt = await provider.getTransactionReceipt({hash: blobTx.hash});
 
-            const numOfBlobs = txBlobs.length;
             const blobGasPrice = receipt.blobGasPrice;
             if (!blobGasPrice) {
+                // Should not happen
                 return failure(new Error(`Blob gas price not found in receipt for transaction ${blobTx.hash}`));
             }
 
+            // Calculate the execution fee per blob
             const executionTxFee = receipt.effectiveGasPrice * receipt.gasUsed;
-            const executionTxFeePerBlob = executionTxFee / BigInt(numOfBlobs);
+            const executionTxFeePerBlob = executionTxFee / BigInt(txBlobs.length);
+
+            // Calculate the blob fee
             const blobFee = blobGasPrice * GAS_PER_BLOB;
-            this.latestBlobFee = blobFee;
 
             for (const blob of txBlobs) {
+                blobFees.push(blobFee); // Save blob fee for all blobs
                 const actualSize = blob.blobAsBytesTrimmed().length;
 
+                // Compute a unique ID based on sender and receiver addresses, caching the result to avoid redundant calculations.
                 const key = `${blobTx.from}_${blobTx.to}`;
                 const id = this.senderReceiverToId.get(key) ?? (() => {
                     const fromBigInt = BigInt(blobTx.from);
                     const toBigInt = BigInt(blobTx.to ?? "0");
 
-                    const id = BigInt((fromBigInt + toBigInt) % BigInt(1000));
+                    const id = BigInt((fromBigInt + toBigInt) % 1000n);
                     this.senderReceiverToId.set(key, id);
                     return id;
                 })();
@@ -119,10 +126,30 @@ export class BlobDataService {
             }
         }
 
-        // TODO: execution fee estimate
-        const blockWithBlobs = new BlockWithBlobs(block.number, block.timestamp, this.latestBlobFee, BigInt(0), blobInfos);
-        this.saveBlockWithBlobs(blockWithBlobs);
+        // Estimate the blob aggregator execution fee using the next base fee and the median 20th percentile reward from the last 10 blocks
+        const feeInfoResult = await getMedianFee(provider);
+        if (feeInfoResult.isFailure()) {
+            return failure(new Error(`Failed to fetch median fee: ${feeInfoResult.unwrapError()}`));
+        }
+        const medianFeeInfo = feeInfoResult.unwrap();
+        const medianExecutionFeeEstimate = medianFeeInfo.gasFeeCap * BLOB_AGG_TX_GAS_USED_ESTIMATE;
 
+        // Compute the average blob fee for the current block if it contains blob transactions
+        if (blobFees.length > 0) {
+            this.latestAvgBlobFee = blobFees.reduce((acc, fee) => acc + fee, 0n) / BigInt(blobFees.length);
+        }
+
+        // Return an error if the average blob fee has not been calculated yet
+        if (this.latestAvgBlobFee === -1n) {
+            // Consider using `baseFeePerBlobGas` and `blobGasUsedRatio` from `eth_feeHistory` for calculation.
+            // This requires a custom `getFeeHistory` implementation since the Viem client doesn't support full data retrieval.
+            // See: https://github.com/wevm/viem/blob/c19ba8d7c572aa8ea19c8616799d1db8675cf11d/src/types/fee.ts#L3
+            return failure(new Error('Cannot calculate average blob fee: no blob transactions found in recent blocks. Waiting for the block with blob transactions...'));
+        }
+
+        const blockWithBlobs = new BlockWithBlobs(block.number, block.timestamp, this.latestAvgBlobFee, medianExecutionFeeEstimate, blobInfos);
+
+        this.saveBlockWithBlobs(blockWithBlobs);
         return success(blockWithBlobs);
     }
 
